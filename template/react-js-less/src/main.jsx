27af9b0cc544da9ce1866878a055ef4669a4bc7c
Xi-duo <1737359096@qq.com>
2024-07-15 13:21:09 +0800

    删除了不必要的依赖和配置

--- template/react-js-less/src/main.jsx
+++ template/react-js-less/src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
-const rootElement = document.getElementById('root')!;
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
